7033d5abf78ee55698e07fd912f675c787d08093
zhangjw <newjunwei@126.com>
2016-03-25 21:13:26 +0800

    init

---
+++ CMakeFiles/shttpd.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_error.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_error.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_method.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_method.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_mine.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_mine.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_parameters.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_parameters.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_request.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_request.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_uri.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_uri.c.o"
+  "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/shttpd_worker.c" "/home/fsdevops/Documents/code/linux (invalid encoding)/linux (invalid encoding)/18/CMakeFiles/shttpd.dir/shttpd_worker.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/shttpd.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shttpd.dir/shttpd.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_error.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_method.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_mine.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_parameters.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_request.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_uri.c.o"
+  "CMakeFiles/shttpd.dir/shttpd_worker.c.o"
+  "shttpd.pdb"
+  "shttpd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shttpd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
